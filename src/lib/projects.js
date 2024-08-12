343e99b8e53411c9ea084bb8d34b2c444bbdc69d
rheap404 <rheaprakash2004@gmail.com>
2024-08-12 22:10:06 +0530

    Added Projects

---
+++ src/lib/projects.js
@@ -0,0 +1,44 @@
+
+import chess from "../images/chess.png";
+import amazon from "../images/amazon.png";
+import kml from "../images/kml.png";
+import baccarat from "../images/baccarat.png";
+
+export const projects = [
+    {
+        title: "Chess AI",
+        link: "https://github.com/rheap404/chess",
+        image:
+        {
+            url: chess,
+            alt: "Chess board image"
+        }
+    },
+     {
+        title: "Full-stack amazon app recreation",
+        link: "https://github.com/rheap404/Full-Stack-Amazon-Website-Recreation",
+        image:
+        {
+            url: amazon,
+            alt: "Full stack amazon web-app"
+        }
+     },
+     {
+        title: "GPS Track Data to KML Converter",
+        link: "https://github.com/rheap404/GPS-Track-Data-to-KML-convertor",
+        image:
+        {
+            url: kml,
+            alt: "Example GPS Track Data image"
+        }
+    },
+     {
+        title: "Baccarat Game",
+        link: "https://github.com/rheap404/Baccarat-game",
+        image:
+        {
+            url: baccarat,
+            alt: "Baccarat game console image"
+        }
+    }
+]
---
+++ src/lib/websites.js
@@ -0,0 +1,15 @@
+
+import drs from "../images/drs-website.png";
+
+export const work = [
+    // DRS Website
+    {
+        title: "DRS Software website",
+        href: "https://drs.software/",
+        image:
+          { url: drs,
+            alt: "DRS website landing page img"
+          }
+
+    }
+]
