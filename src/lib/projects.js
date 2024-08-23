bb288c82a2c0d56e12190329a4cdd08be6ae970b
rheap404 <rheaprakash2004@gmail.com>
2024-08-23 17:46:14 +0530

    Added Tags to projects

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -3,11 +3,14 @@ import chess from "../images/chess.png";
 import amazon from "../images/amazon.png";
 import kml from "../images/kml.png";
 import baccarat from "../images/baccarat.png";
+import server from "../images/client-server.png";
+import qrCode from "../images/QR.png";
 
 export const projects = [
     {
         title: "Chess AI",
         link: "https://github.com/rheap404/chess",
+        tags: ["Java", "AI", "MiniMax", "System Design"],
         image:
         {
             url: chess,
@@ -17,6 +20,7 @@ export const projects = [
      {
         title: "Full-stack amazon app recreation",
         link: "https://github.com/rheap404/Full-Stack-Amazon-Website-Recreation",
+        tags: ["React", "Node Js", "Typescript", "Javascript"],
         image:
         {
             url: amazon,
@@ -26,6 +30,7 @@ export const projects = [
      {
         title: "GPS Track Data to KML Converter",
         link: "https://github.com/rheap404/GPS-Track-Data-to-KML-convertor",
+        tags: ["Java", "KML files"],
         image:
         {
             url: kml,
@@ -33,12 +38,33 @@ export const projects = [
         }
     },
      {
-        title: "Baccarat Game",
-        link: "https://github.com/rheap404/Baccarat-game",
+        title: "Client-Server",
+        link: "https://github.com/rheap404/Server-Client",
+        tags: ["Java", "Networks", "FTP"],
         image:
         {
-            url: baccarat,
-            alt: "Baccarat game console image"
+            url: server,
+            alt: "Client-Server Architechture image"
         }
-    }
+    },
+    {
+       title: "Baccarat Game",
+       link: "https://github.com/rheap404/Baccarat-game",
+       tags: ["Java", "Javascript", "OOP", "System Design"],
+       image:
+       {
+           url: baccarat,
+           alt: "Baccarat game console image"
+       }
+   },
+   {
+      title: "QR Code Generator",
+      link: "https://github.com/rheap404/QR-Generator",
+      tags: ["Python", "GUI"],
+      image:
+      {
+          url: qrCode,
+          alt: "QR code generator"
+      }
+  }
 ]
