71df3e70c1ef99850ed8c45526ab1c62f819ed25
rheap404 <rheaprakash2004@gmail.com>
2025-03-16 15:17:22 +0000

    Changed structure

--- src/lib/websites.js
+++ src/lib/websites.js
@@ -4,12 +4,17 @@ import drs from "../images/drs-website.png";
 export const work = [
     // DRS Website
     {
-        title: "DRS Software website",
+        title: "DRS Software",
         href: "https://drs.software/",
         image:
           { url: drs,
             alt: "DRS website landing page img"
-          }
+          },
+        points: ["Developed a high-performance, visually engaging static website using Astro, ensuring fast load times and responsiveness.",
+                "Utilized Astro for efficient static site generation and performance optimization",
+                "Strengthened time management and communication skills through collaborative work."
+        ]
+
 
     }
 ]
